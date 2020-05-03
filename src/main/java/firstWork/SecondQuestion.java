40ceb3fd44abf029986ea27bc8bb8e8477489931
zm1105 <827556302@qq.com>
2020-05-03 18:57:08 +0800

    第一次作业第二题

--- src/main/java/firstWork/SecondQuestion.java
+++ src/main/java/firstWork/SecondQuestion.java
@@ -4,7 +4,7 @@ package firstWork;
  * @author : zhoumin
  * @data :  2020/5/3 17:30
  */
-public class Secondquestion {
+public class SecondQuestion {
   //编程找出 1000 以内的所有完数并打印出来。 所谓完数就是一个数恰好等于它的因子之和，如：6=1＋2＋3
   public static void main(String[] args) {
     for (int i = 1; i <= 1000; i++) {
